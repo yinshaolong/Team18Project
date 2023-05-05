425b38feb8f29775907b41a439f4d717ca922c36
fluffybrownie99 <115048106+fluffybrownie99@users.noreply.github.com>
2023-05-04 20:33:21 -0700

    added static css file loading for web pages

--- project/members/models.py
+++ project/members/models.py
@@ -1,24 +1,46 @@
 from django.db import models
-
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 # Create your models here.
 #This is the main data base. We can add new members to our database using predefined attributes here
 #A model is simply a table in your database
 #in the 'Project' directory, you can run 'py manage.py makemigrations members' and then 'py manage.py migrate' to create the tables based on the models below
 #The 
 
-class User(models.Model):
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+        """
+        Creates and saves a User with the given email and password.
+        """
+        if not email:
+            raise ValueError('Users must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
     user_id = models.IntegerField(primary_key=True,serialize=False, verbose_name= 'User ID', unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     username = models.CharField(max_length=255, unique=True)
-    password = models.CharField(max_length=255)
-    email = models.EmailField(max_length=255, unique=True)
+    email = models.EmailField(verbose_name = 'Email Address',max_length=255, unique=True)
     
+    objects = UserManager()
     
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['first_name', 'last_name', 'email']
+
 class Location(models.Model):
     location_id = models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='Location ID',unique=True)
-    latitude = models.IntegerField()
-    longitude= models.IntegerField()
+    latitude = models.DecimalField(max_digits=9, decimal_places=6)
+    longitude= models.DecimalField(max_digits=9, decimal_places=6)
 
 class Itinerary(models.Model):
     itinerary_id = models.IntegerField(primary_key=True,serialize=False, verbose_name= 'Itinerary ID', unique=True)
@@ -27,10 +49,20 @@ class Itinerary(models.Model):
     travel_time = models.DateTimeField()
     
 class Business(models.Model):
+    LODGING = 'Lodging'
+    RESTAURANTS = 'Restaurants'
+    THINGS_TO_DO = 'Things To Do'
+    BUSINESS_TYPES = [
+        (LODGING, 'Lodging'),
+        (RESTAURANTS, 'Restaurants'),
+        (THINGS_TO_DO, 'Things to do')
+    ]
     business_id = models.IntegerField(primary_key=True, serialize=False, verbose_name='Business ID', unique=True)
     business_name = models.CharField(max_length=255)
+    business_type = models.CharField(max_length=12, choices=BUSINESS_TYPES)
     pricerange = models.IntegerField()
     country = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+
--- project/project/settings.py
+++ project/project/settings.py
@@ -116,9 +116,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'members.User'
