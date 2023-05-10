70d841ad182ba02d64646499cb707fe7c4408653
Ramith <ramithr11@gmail.com>
2023-05-10 00:52:35 -0700

    fixed static routing from login page

--- project/members/views.py
+++ project/members/views.py
@@ -1,52 +1,55 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, logout, login as auth_login
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.template import loader
 from django.contrib.auth.models import User
-from .forms import CreateForm
 # Create your views here.
 #These functions will be called to render templates made in the 'templates' directory
 from django.http import HttpResponse
 
 
 def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('home')
     if request.method == 'POST':
         username = request.POST["username"]
         password = request.POST["password"]
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user)
+            auth_login(request, user)
+            messages.success(request, f"User has been authenticated. Welcome {user.get_username()}!")
             return redirect('home')
         else:
-            messages.success(request, "There was an error logging in. Please try again...")
-            return render(request, 'registration.login.html', {}) 
-
+            messages.error(request, "There was an error logging in. Please try again...")
+            return redirect('login')
     else:
         return render(request, 'registration/login.html', {})
 
 def register_user(request):
     if request.method == 'POST':
-        form = CreateForm(request.POST)
+        form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get("username")
             raw_password = form.cleaned_data.get("password1")
             user = authenticate(username=username, password=raw_password)
-            login(request, user)
+            auth_login(request, user)
             return redirect('home')
         else:
             messages.error(request, "There was an error creating the user. Please try again.")
     else:
-        form = CreateForm()
+        form = UserCreationForm()
     return render(request, 'registration/createuser.html', {'form': form})
 
 def signout(request):
     if request.user.is_authenticated:
         logout(request)
         messages.success(request, "You have been logged out!")
-    return redirect('login')
+        return redirect('logout')
+    else:
+        return redirect('home')
 
 
 def home(request):
